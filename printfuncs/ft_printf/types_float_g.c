038703c119e6e35823c995e853b104eb58512f5e
Elena <elena@Elenas-MacBook-Air.local>
2018-01-06 02:42:29 +0200

    likely e case for %g

--- printfuncs/ft_printf/types_float_g.c
+++ printfuncs/ft_printf/types_float_g.c
@@ -13,6 +13,33 @@
 #include "../../includes/ft_printf.h"
 #include "libft.h"
 
+static void	print_e_exp(t_params *p, int expon, char c)
+{
+	size_t			savelen_exp;
+
+	print_symbol(p, c);
+	print_symbol(p, expon < 0 ? '-' : '+');
+	expon = expon < 0 ? -expon : expon;
+	if (expon == 0)
+		print_str(p, "00", 1);
+	else if (expon < 10)
+	{
+		print_symbol(p, '0' + expon / 10);
+		print_symbol(p, '0' + expon % 10);
+	}
+	else
+	{
+		savelen_exp = p->toprint->len;
+		while (expon > 0)
+		{
+			print_symbol(p, '0' + expon % 10);
+			expon /= 10;
+		}
+		rev_str(&p->toprint->str[savelen_exp],
+			&p->toprint->str[p->toprint->len - 1]);
+	}
+}
+
 static void	type_gbg(va_list *ap, t_params *p, char c, _Bool is_cap)
 {
 	long double		num;
@@ -29,27 +56,44 @@ static void	type_gbg(va_list *ap, t_params *p, char c, _Bool is_cap)
 		return ;
 	f = !f ? init_t_float() : f;
 	set_float(f, num);
-	expon = f->point;
-	saveprec = p->precision;
-	p->precision -= f->point - 1;
-	round_float(f, p);
-	p->precision = saveprec;
 	i = 0;
 	while (f->num[i] == '0')
 		i++;
-	if (i == f->size)
-		i = 1;
-	expon -= i + 1;
+	expon = f->point - i - 1;
+	print_sign_pref(p);
 	//printf("expon %d precision %d\n", expon, p->precision);
 	if (expon < -4 || expon >= p->precision)
 	{
-		;//print exp
+		saveprec = p->precision;
+		if (i == f->size)
+			i = 1;
+		p->precision += i - f->point;
+		round_float(f, p);
+		p->precision = saveprec;
+		i -= f->num[i - 1] == '0' ? 0 : 1;
+		expon += f->num[i - 1] == '0' ? 0 : 1;
+		print_symbol(p, f->num[i++]);
+		j = i + p->precision;
+		while (f->num[--j] == '0')
+			p->precision--;
+		if (p->precision > 1)
+		{
+			print_symbol(p, '.');
+			f->size = p->precision + (f->num[0] == '0' ? 1 : 0);
+			j = 0;
+			while (++j < p->precision)	
+				print_symbol(p, f->num[i + j - 1]);		
+		}
+		print_e_exp(p, expon, c);
 	}
 	else
 	{
-		print_sign_pref(p);
+		saveprec = p->precision;
+		p->precision -= f->point - 1;
+		round_float(f, p);
+		p->precision = saveprec;
 		i = 0;
-		while (f->num[i] == '0' && i < f->point - 1)
+		while (f->num[i] == '0' && i < f->point)
 			i++;
 		i--;
 		f->size = p->precision + (f->num[0] == '0' ? 1 : 0);
